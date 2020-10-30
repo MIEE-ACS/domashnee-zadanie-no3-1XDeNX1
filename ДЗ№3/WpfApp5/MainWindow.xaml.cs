ffb7a51e7b3aaccab67affeb713c495b74d8153b
1XDeNX1 <57237634+1XDeNX1@users.noreply.github.com>
2020-10-30 21:52:14 +0300

    Выполнил Домашнее задание.

---
+++ ДЗ№3/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] str = Tb1.Text.Split('.','!','?');
+            int a = 0;
+            for(int i = 0; i< str.Length; i++) 
+            {
+                if (str[i] != "") 
+                {
+                    a++;
+                }
+            }
+            Tb2.Text = $"Кол-во предложений: {a}";
+        }
+    }
+}
